9e15cd877b6314fcfe2cce19251205ba58c50b5a
Muhab Tomoum <muhabtomoum@gmail.com>
2023-04-08 18:27:49 -0700

    update readme remove unusefull files

---
+++ src/logzilla/__init__.py
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------------------------------
+# Copyright (c) Muhab Tomoum. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+# ---------------------------------------------------------------------------------
+from __future__ import annotations
+
+__version__ = "0.1.0"
---
+++ src/logzilla/logzilla.py
@@ -0,0 +1,29 @@
+# ---------------------------------------------------------------------------------
+# Copyright (c) Muhab Tomoum. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+# ---------------------------------------------------------------------------------
+"""This is a Sample Python file."""
+
+
+from __future__ import annotations
+
+
+def hello_world(i: int = 0) -> str:
+    """Doc String."""
+    print("hello world")
+    return f"string-{i}"
+
+
+def good_night() -> str:
+    """Doc String."""
+    print("good night")
+    return "string"
+
+
+def hello_goodbye():
+    hello_world(1)
+    good_night()
+
+
+def logzilla():
+    hello_goodbye()
